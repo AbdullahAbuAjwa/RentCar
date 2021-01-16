01ef78a492a97f59346c83273cffdd638f27a0bc
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2021-01-17 01:39:09 +0200

    major update for all project

--- app.js
+++ app.js
@@ -41,26 +41,29 @@ app.get('/rent_info', (req, res) => {
   res.render('rent_info', { title: 'Information' });
 });
 app.get('/all_cars', (req, res) => {
+  global.dates = { from: '', to: '' };
   Car.find().then((result) => {
     res.render('all_cars', { title: 'All Cars', cars: result });
   }).catch((err) => {
     res.status(404).render('404', { title: 'Error' });
   });
 });
-app.get('/rent_info_details', (req, res) => {
-  res.render('rent_info_details', { title: 'Information Details' });
-});
-app.get('/user_info_page', (req, res) => {
-  res.render('user_info_page', { title: 'User Information' });
+app.post('/rent_info_details', (req, res) => {
+  User.find({ email: req.body.email }).then((result) => {
+    res.render('rent_info_details', { title: 'Information Details', user: result[0] });
+  }).catch((err) => {
+    res.status(404).render('404', { title: 'Error' });
+  });
 });
 app.get('/user_info_page/:id', (req, res) => {
   Car.findById(req.params.id).then(result => {
-    res.render('user_info_page', { title: 'User Information', car: result });
+    res.render('user_info_page', { title: 'User Information', car: result, globa: global.dates });
   }).catch(err => {
     res.status(404).render('404', { title: 'Error' });
   });
 });
 app.post('/search_result', (req, res) => {
+  global.dates = { from: req.body.receive_date, to: req.body.arrive_date };
   Car.find({ available: true }).then((result) => {
     res.render('search_result', { title: 'Search Result', cars: result });
   }).catch((err) => {
@@ -75,19 +78,50 @@ app.post('/search_result/filter', (req, res) => {
   });
 });
 app.post('/user/new', (req, res) => {
-  Car.findByIdAndUpdate(req.body.carid, { available: false }).then((result) => {
-    const user = new User({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      email: req.body.email,
-      phone: req.body.phone,
-      birthdate: req.body.birthdate,
-      idnumber: req.body.idnumber,
-      city: req.body.city
+  Car.findByIdAndUpdate(req.body.carid, { available: false, from: req.body.from, to: req.body.to }).then((result1) => {
+    const underdata = { id: req.body.carid, name: result1.name, cost: result1.cost, from: new Date(req.body.from), to: new Date(req.body.to) };
+    User.findOneAndUpdate({ email: req.body.email }, { $addToSet: { underrents: underdata } }).then(result2 => {
+      if (result2 == null) {
+        // add new user
+        const user = new User({
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          email: req.body.email,
+          phone: req.body.phone,
+          birthdate: req.body.birthdate,
+          idnumber: req.body.idnumber,
+          city: req.body.city,
+          underrents: [underdata],
+          lastrents: []
+        });
+        user.save().then((result) => {
+          res.render('rent_info_details', { title: 'Information Details', carid: req.body.carid, user: user });
+        }).catch((err) => {
+          res.status(404).render('404', { title: 'Error' });
+        });
+      } else {
+        User.findById(result2.id).then(result => {
+          res.render('rent_info_details', { title: 'Information Details', carid: req.body.carid, user: result});
+        });
+      }
+    }).catch(err => {
+      res.status(404).render('404', { title: 'Error' });
     });
-    user.save().then((result) => {
-      res.render('rent_info_details', { title: 'Information Details', carid : req.body.carid, users:user });
-    }).catch((err) => {
+  }).catch(err => {
+    res.status(404).render('404', { title: 'Error' });
+  });
+});
+// move user underrent car to lastrent
+app.post('/edit_car', (req, res) => {
+  User.findByIdAndUpdate(req.body.userid, {
+    $push: { lastrents: { name: req.body.name, cost: req.body.cost, from: new Date(req.body.from), to: new Date(req.body.to) } },
+    $pull: { underrents: {id : req.body.carid} }
+  }).then(result => {
+    Car.findByIdAndUpdate(req.body.carid,{available : true}).then(result => {
+      User.findById(req.body.userid).then(result => {
+        res.render('rent_info_details', { title: 'Information Details', carid: req.body.carid, user: result});
+      });
+    }).catch(err => {
       res.status(404).render('404', { title: 'Error' });
     });
   }).catch(err => {
